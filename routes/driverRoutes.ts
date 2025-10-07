d13827c2760e00e7a6c9ed3fdea3f85195db6771
Abbas Dalal <91382081+Abbas19402@users.noreply.github.com>
2025-10-07 16:30:59 +0530

    Revert "Feature/admin"

--- routes/driverRoutes.ts
+++ routes/driverRoutes.ts
@@ -4,12 +4,11 @@ import express from 'express';
 import { signup } from '../controllers/DriverController.ts';
 import { login } from '../controllers/DriverController.ts';
 import { logout } from '../controllers/DriverController.ts';
-import { getRide } from '../controllers/DriverController.ts';
+
 const router = express.Router();
 
 // Signup route
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
-router.get('/getride/:trackingId', getRide);
 export default router;
